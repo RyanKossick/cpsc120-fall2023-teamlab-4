f0a0265f2dca497beedd151f326bfaddbe8d89d4
NighLow <143144090+NighLow@users.noreply.github.com>
2023-10-02 22:58:21 -0700

    Third Commit

--- part-1/units.cc
+++ part-1/units.cc
@@ -17,15 +17,15 @@ int main(int argc, char const *argv[]) {
   std::cout << "Enter ml: ";
   std::cin >> milliliter_value;
 
-  double tsp_value{MlToTsp(milliliter_value)};
-  double tbsp_value{MlToTbsp(milliliter_value)};
+  double tsp_value = MlToTsp(milliliter_value);
+  double tbsp_value = MlToTbsp(milliliter_value);
   double ounce_value = MlToOz(milliliter_value);
   double cup_value = MlToCup(milliliter_value);
 
-  std::cout << "ml = " << tsp_value;
-  std::cout << "tsp = " << tbsp_value;
-  std::cout << "tbsp = " << ounce_value;
-  std::cout << "oz = " << cup_value;
+  std::cout << milliliter_value << "ml = " << tsp_value;
+  std::cout << milliliter_value << "tsp = " << tbsp_value;
+  std::cout << milliliter_value << "tbsp = " << ounce_value;
+  std::cout << milliliter_value << "oz = " << cup_value;
   
   return 0;
 }
